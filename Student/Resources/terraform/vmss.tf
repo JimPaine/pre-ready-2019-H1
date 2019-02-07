f2b9788fcc52a0c5616dd15e66f445555a1766dd
Jim Paine <jim@jimpaine.io>
2019-02-07 17:08:22 +0000

    another typo :(

--- Student/Resources/terraform/vmss.tf
+++ Student/Resources/terraform/vmss.tf
@@ -31,7 +31,7 @@ resource "azurerm_monitor_diagnostic_setting" "vmssip" {
 
     retention_policy {
       enabled = false
-      days = "${var.retention_policy}"
+      days = "${var.retentionPolicy}"
     }
   }
 }
