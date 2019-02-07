bac88f5a18a39e64dacf38c8001f9ae5f6a6725c
Jim Paine <jim@jimpaine.io>
2019-02-07 19:52:15 +0000

    updated azuread provider

--- Student/Resources/terraform/providers.tf
+++ Student/Resources/terraform/providers.tf
@@ -7,7 +7,11 @@ provider "azurerm" {
 }
 
 provider "azuread" {
-  version = "~>0.1.0"
+  version = "~>0.1.0"  
+  subscription_id = "${var.subscription_id}"
+  client_id       = "${var.client_id}"
+  client_secret   = "${var.client_secret}"
+  tenant_id       = "${var.tenant_id}"
 }
 
 provider "random" {
