8052482b096c91ac6cb04b77bc0fee0ede66e655
Jim Paine <jim@jimpaine.io>
2019-02-08 14:40:07 +0000

    switch app insights to arm

--- Student/Resources/terraform/app_insights.tf
+++ Student/Resources/terraform/app_insights.tf
@@ -1,79 +1,139 @@
-resource "azurerm_application_insights" "hack" {
-  name                = "${var.envPrefixName}hackAppInsights"
-  location            = "${azurerm_resource_group.hack.location}"
-  resource_group_name = "${azurerm_resource_group.hack.name}"
-  application_type    = "Web"
-}
-
 resource "azurerm_template_deployment" "webtest" {
   name                = "${var.envPrefixName}webtest"
   resource_group_name = "${azurerm_resource_group.hack.name}"
 
   template_body = <<DEPLOY
-{
-    "$schema": "http://schema.management.azure.com/schemas/2014-04-01-preview/deploymentTemplate.json",
+  {
+    "$schema": "http://schema.management.azure.com/schemas/2015-01-01/deploymentTemplate.json#",
     "contentVersion": "1.0.0.0",
     "parameters": {
-        "appLocation": {
-            "type": "String"
-        },
         "appName": {
-            "type": "String"
+            "type": "string",
+            "metadata": {
+                "description": "Enter the application name."
+            }
         },
-        "subscription_id": {
-            "type": "String"
+        "appLocation": {
+            "type": "string",
+            "defaultValue": "East US",
+            "metadata": {
+                "description": "Enter the application location."
+            }
+        },
+        "testlocations": {
+            "type": "array",
+            "defaultValue": [
+                {
+                    "Id": "us-il-ch1-azr"
+                },
+                {
+                    "Id": "us-ca-sjc-azr"
+                },
+                {
+                    "Id": "us-tx-sn1-azr"
+                }
+            ],
+            "metadata": {
+                "description": ""
+            }
         },
         "webSrvPublicIP": {
-            "type": "String"
+            "type": "string",
+            "metadata": {
+                "description": ""
+            }
+        },
+        "email": {
+            "type": "string",
+            "defaultValue": "admin@somewhere.com",
+            "metadata": {
+                "description": ""
+            }
         }
     },
-    "variables": {        
-        "pingguid": "[guid(parameters('subscription_id'))]",        
-        "pingexpected": 200,
-        "pingname": "eShopPingTest"
+    "variables": {
+        "appType": "web",
+        "priceCode": 1,
+        "dailyQuota": 100,
+        "dailyQuotaResetTime": 24,
+        "warningThreshold": 90,
+        "priceArray": [
+            "Basic",
+            "Application Insights Enterprise"
+        ],
+        "pricePlan": "[take(variables('priceArray'),variables('priceCode'))]",
+        "billingplan": "[concat(parameters('appName'),'/', variables('pricePlan')[0])]",
+        "pingname": "eShopPingTest",
+        "pingguid": "[guid(subscription().id)]",
+        "pingexpected": 200
     },
     "resources": [
+        {
+            "type": "microsoft.insights/components",
+            "kind": "[variables('appType')]",
+            "name": "[parameters('appName')]",
+            "apiVersion": "2014-04-01",
+            "location": "[parameters('appLocation')]",
+            "tags": {},
+            "properties": {
+                "ApplicationId": "[parameters('appName')]"
+            },
+            "dependsOn": []
+        },
+        {
+            "name": "[variables('billingplan')]",
+            "type": "microsoft.insights/components/CurrentBillingFeatures",
+            "location": "[parameters('appLocation')]",
+            "apiVersion": "2015-05-01",
+            "dependsOn": [
+                "[resourceId('microsoft.insights/components', parameters('appName'))]"
+            ],
+            "properties": {
+                "CurrentBillingFeatures": "[variables('pricePlan')]",
+                "DataVolumeCap": {
+                    "Cap": "[variables('dailyQuota')]",
+                    "WarningThreshold": "[variables('warningThreshold')]",
+                    "ResetTime": "[variables('dailyQuotaResetTime')]"
+                }
+            }
+        },
         {
             "name": "[variables('pingname')]",
             "apiVersion": "2015-05-01",
             "type": "microsoft.insights/webtests",
-            "location": "[parameters('appLocation')]",
-            "dependsOn": [
-                "[resourceId('Microsoft.Insights/components', parameters('appName'))]"
-            ],
             "tags": {
-                "[concat('hidden-link:', resourceId('Microsoft.Insights/components', parameters('appName')))]": "Resource"
+                "[concat('hidden-link:', resourceId('microsoft.insights/components/', parameters('appName')))]": "Resource"
             },
+            "location": "[resourceGroup().location]",
+            "dependsOn": [
+                "[concat('microsoft.insights/components/', parameters('appName'))]"
+            ],
             "properties": {
-                "Name": "[variables('pingname')]",
-                "Description": "[variables('pingname')]",
+                "Name": "eShopPingTest",
+                "Description": "eShopPingTest",
                 "Enabled": true,
                 "Frequency": 300,
                 "Timeout": 30,
                 "Kind": "ping",
-                "Locations": [
-                    {
-                        "Id": "us-il-ch1-azr"
-                    },
-                    {
-                        "Id": "us-ca-sjc-azr"
-                    },
-                    {
-                        "Id": "us-tx-sn1-azr"
-                    }
-                ],
+                "Locations": "[parameters('testlocations')]",
                 "Configuration": {
                     "WebTest": "[concat('<WebTest Name=\"', variables('pingname'), '\"',  ' Id=\"', variables('pingguid') ,'\"    Enabled=\"True\" CssProjectStructure=\"\" CssIteration=\"\" Timeout=\"0\" WorkItemIds=\"\" xmlns=\"http://microsoft.com/schemas/VisualStudio/TeamTest/2010\" Description=\"\" CredentialUserName=\"\" CredentialPassword=\"\" PreAuthenticate=\"True\" Proxy=\"default\" StopOnError=\"False\" RecordedResultFile=\"\" ResultsLocale=\"\">        <Items>        <Request Method=\"GET\" Guid=\"a5f10126-e4cd-570d-961c-cea43999a200\" Version=\"1.1\" Url=\"', parameters('webSrvPublicIP') ,'\" ThinkTime=\"0\" Timeout=\"300\" ParseDependentRequests=\"True\" FollowRedirects=\"True\" RecordResult=\"True\" Cache=\"False\" ResponseTimeGoal=\"0\" Encoding=\"utf-8\" ExpectedHttpStatusCode=\"', variables('pingexpected') ,'\" ExpectedResponseUrl=\"\" ReportingName=\"\" IgnoreHttpStatusCode=\"False\" /></Items></WebTest>')]"
                 },
-                "SyntheticMonitorId": "[variables('pingname')]"
+                "SyntheticMonitorId": "eShopPingTest"
             }
         }
-    ]
+    ],
+    "outputs": {
+        "AIresourceID": {
+            "type": "string",
+            "value": "[resourceId('microsoft.insights/components', parameters('appName'))]"
+        }
+    }
 }
 DEPLOY
 
   parameters {
-    "appName"     = "${azurerm_application_insights.hack.app_id}"
+    "appName"     = "${var.envPrefixName}insights"
     "appLocation" = "${azurerm_resource_group.hack.location}"
     "subscription_id" = "${var.subscription_id}"
     "webSrvPublicIP" = "${azurerm_public_ip.vmss.fqdn}"
