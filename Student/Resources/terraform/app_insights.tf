b606b1e2b8720feb130c9e240fe9171a545b1dba
Jim Paine <jim@jimpaine.io>
2019-02-08 11:34:00 +0000

    switched to use app id

--- Student/Resources/terraform/app_insights.tf
+++ Student/Resources/terraform/app_insights.tf
@@ -27,19 +27,7 @@ resource "azurerm_template_deployment" "webtest" {
             "type": "String"
         }
     },
-    "variables": {
-        "testlocations": [
-                {
-                    "Id": "us-il-ch1-azr"
-                },
-                {
-                    "Id": "us-ca-sjc-azr"
-                },
-                {
-                    "Id": "us-tx-sn1-azr"
-                }
-            ],
-        
+    "variables": {        
         "pingguid": "[guid(parameters('subscription_id'))]",        
         "pingexpected": 200,
         "pingname": "eShopPingTest"
@@ -63,7 +51,17 @@ resource "azurerm_template_deployment" "webtest" {
                 "Frequency": 300,
                 "Timeout": 30,
                 "Kind": "ping",
-                "Locations": "[variables('testlocations')]",
+                "Locations": "[
+                    {
+                        "Id": "us-il-ch1-azr"
+                    },
+                    {
+                        "Id": "us-ca-sjc-azr"
+                    },
+                    {
+                        "Id": "us-tx-sn1-azr"
+                    }
+                ]",
                 "Configuration": {
                     "WebTest": "[concat('<WebTest Name=\"', variables('pingname'), '\"',  ' Id=\"', variables('pingguid') ,'\"    Enabled=\"True\" CssProjectStructure=\"\" CssIteration=\"\" Timeout=\"0\" WorkItemIds=\"\" xmlns=\"http://microsoft.com/schemas/VisualStudio/TeamTest/2010\" Description=\"\" CredentialUserName=\"\" CredentialPassword=\"\" PreAuthenticate=\"True\" Proxy=\"default\" StopOnError=\"False\" RecordedResultFile=\"\" ResultsLocale=\"\">        <Items>        <Request Method=\"GET\" Guid=\"a5f10126-e4cd-570d-961c-cea43999a200\" Version=\"1.1\" Url=\"', parameters('webSrvPublicIP') ,'\" ThinkTime=\"0\" Timeout=\"300\" ParseDependentRequests=\"True\" FollowRedirects=\"True\" RecordResult=\"True\" Cache=\"False\" ResponseTimeGoal=\"0\" Encoding=\"utf-8\" ExpectedHttpStatusCode=\"', variables('pingexpected') ,'\" ExpectedResponseUrl=\"\" ReportingName=\"\" IgnoreHttpStatusCode=\"False\" /></Items></WebTest>')]"
                 },
@@ -75,7 +73,7 @@ resource "azurerm_template_deployment" "webtest" {
 DEPLOY
 
   parameters {
-    "appName"     = "${azurerm_application_insights.hack.name}"
+    "appName"     = "${azurerm_application_insights.hack.app_id}"
     "appLocation" = "${azurerm_resource_group.hack.location}"
     "subscription_id" = "${var.subscription_id}"
     "webSrvPublicIP" = "${azurerm_public_ip.vmss.fqdn}"
